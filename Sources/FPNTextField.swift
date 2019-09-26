2702cb4ee536a31b5221a9331a7e8c320159b2c5
Muhammad Umair Gillani <umair.gillani@ucp.edu.pk>
2019-09-26 05:45:31 +0500

    height constraint update

--- Sources/FPNTextField.swift
+++ Sources/FPNTextField.swift
@@ -177,7 +177,7 @@ open class FPNTextField: UITextField, FPNCountryPickerDelegate, FPNDelegate {
 		let newRect: CGRect = CGRect(x: leftViewFrame.minX, y: leftViewFrame.minY, width: width, height: height)
         
         if isRtl {
-            let Rect: CGRect = CGRect(x: leftViewFrame.minX, y: leftViewFrame.minY, width: height, height: 20)
+            let Rect: CGRect = CGRect(x: leftViewFrame.minX, y: leftViewFrame.minY, width: 20, height: height)
             return Rect
         }
 
